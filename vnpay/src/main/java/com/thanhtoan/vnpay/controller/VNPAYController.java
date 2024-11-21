582c3ca0a2697215281bdf8b6b55da710552160c
ledinhloc <ledinhloc7@gmail.com>
2024-11-21 15:44:30 +0700

    update

---
+++ vnpay/src/main/java/com/thanhtoan/vnpay/controller/VNPAYController.java
@@ -0,0 +1,54 @@
+package com.thanhtoan.vnpay.controller;
+
+import com.thanhtoan.vnpay.config.VNPAYService;
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
+public class VNPAYController {
+    @Autowired
+    private VNPAYService vnPayService;
+
+    @GetMapping
+    public String home(){
+
+//        System.out.println("-------------fix---------------");
+        return "createOrder";
+    }
+
+    // Chuyển hướng người dùng đến cổng thanh toán VNPAY
+    @PostMapping("/submitOrder")
+    public String submitOrder(@RequestParam("amount") int orderTotal,
+                              @RequestParam("orderInfo") String orderInfo,
+                              HttpServletRequest request){
+        String baseUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort();
+        String vnpayUrl = vnPayService.createOrder(request, orderTotal, orderInfo, baseUrl);
+        return "redirect:" + vnpayUrl;
+    }
+
+    // Sau khi hoàn tất thanh toán, VNPAY sẽ chuyển hướng trình duyệt về URL này
+    @GetMapping("/vnpay-payment-return")
+    public String paymentCompleted(HttpServletRequest request, Model model){
+        int paymentStatus =vnPayService.orderReturn(request);
+
+        String orderInfo = request.getParameter("vnp_OrderInfo");
+        String paymentTime = request.getParameter("vnp_PayDate");
+        String transactionId = request.getParameter("vnp_TransactionNo");
+        String totalPrice = request.getParameter("vnp_Amount");
+
+        model.addAttribute("orderId", orderInfo);
+        model.addAttribute("totalPrice", totalPrice);
+        model.addAttribute("paymentTime", paymentTime);
+        model.addAttribute("transactionId", transactionId);
+
+        return paymentStatus == 1 ? "orderSuccess" : "orderFail";
+    }
+}
